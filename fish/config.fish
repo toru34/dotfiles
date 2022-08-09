0dce2fba7bf81eb3eed294993d9186e01dc6844f
Toru Fujino <toru.fb34@gmail.com>
2022-08-09 18:44:37 +0900

    update fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,3 @@
-set fish_greeting
-
 # Aliases
 alias c="clear"
 alias l="ls -alh"
@@ -8,9 +6,19 @@ alias t="tree"
 alias v="nvim"
 alias cat="bat"
 alias vim="nvim"
+alias ga="git add"
 alias gs="git status"
+alias gm="git commit -m"
+alias gpu="git push origin"
+alias gpl="git pull origin"
 alias u="brew upgrade --greedy && brew cleanup && conda update --all -y && conda clean -a -y && rustup update"
 
 # Paths
 set -gx PATH /opt/homebrew/bin $PATH
 set -gx PATH /Users/toru34/.cargo/bin $PATH
+
+# Conda
+eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+
+# Prompt
+set fish_greeting
