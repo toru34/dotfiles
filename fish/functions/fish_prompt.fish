58026ee998ca48241f33bed77502f26f6a8b8a23
Toru Fujino <toru.fb34@gmail.com>
2022-08-13 19:04:17 +0900

    remove fish_prompt

--- fish/functions/fish_prompt.fish
+++
@@ -1,3 +0,0 @@
-function fish_prompt
-    printf '%s%s@%s:%s%s ' (set_color c7c7c7)(echo $CONDA_PROMPT_MODIFIER) (set_color brgreen)(echo $USER) (set_color brcyan)(prompt_hostname) (set_color c7c7c7)(prompt_pwd) (set_color c7c7c7)(echo '>')
-end
